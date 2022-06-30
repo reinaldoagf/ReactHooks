d9a1c66e34c1d5df1e18f6aa55f5aa5297e173f9
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-06-30 15:52:13 -0400

    Ajuste boton de añadir a favoritos

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -60,12 +60,15 @@ const Characters = () => {
                   <div className="text-lg text-white font-light">
                     Episodios: {e.episode.length}
                   </div>
-                  <button onClick={() => onAdd(e)} href="#" className={`rounded-full text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300 ${e.gender === "male" ? "bg-gray-800" : "bg-purple-900"}`}>
+                  {
+                    !favorites.favorites.find(f => f.id === e.id) && (<button onClick={() => onAdd(e)} href="#" className={`rounded-full text-white hover:bg-white hover:text-purple-900 hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300 ${e.gender === "male" ? "bg-gray-800" : "bg-purple-900"}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-current m-auto">
                       <line x1="12" y1="5" x2="12" y2="19"></line>
                       <line x1="5" y1="12" x2="19" y2="12"></line>
                     </svg>
-                  </button>
+                  </button>)
+                  }
+                  
                 </div>
               </div>
             </div>
